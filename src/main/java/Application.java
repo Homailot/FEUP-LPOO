c00cdba0c0fb55abd73e7be3a56b5240b2c8172c
Nuno Alves <nyansword@gmail.com>
2021-02-24 10:47:36 +0000

    Done 6

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,31 +1,12 @@
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
         try {
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            Terminal terminal = new DefaultTerminalFactory()
-                    .setInitialTerminalSize(terminalSize)
-                    .createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
+            Game newGame = new Game();
+            newGame.run();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
