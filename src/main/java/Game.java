aeda8667544ea35371d8ad2ef19b5e8f70321499
Nuno Alves <nyansword@gmail.com>
2021-02-24 11:08:40 +0000

    Done 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,19 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() {
         try {
@@ -26,13 +31,36 @@ public class Game {
         }
     }
 
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp -> y--;
+            case ArrowDown -> y++;
+            case ArrowLeft -> x--;
+            case ArrowRight -> x++;
+        }
+    }
+
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+
+
+            KeyType keyType = key.getKeyType();
+            if(keyType == KeyType.Character && key.getCharacter() == 'q') {
+                screen.close();
+            } else if (keyType == KeyType.EOF) {
+                break;
+            }
+        }
+
+
     }
 }
