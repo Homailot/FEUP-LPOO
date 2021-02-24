483c1128d5de5c32d9ebc679b5f1fd5c7eacbd90
Nuno Alves <nyansword@gmail.com>
2021-02-24 12:12:56 +0000

    Done 12

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -3,23 +3,22 @@ import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.screen.Screen;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int width;
     private int height;
     private Hero hero;
-
-    public Arena() {
-        hero = new Hero(10, 10);
-        width = 40;
-        height = 20;
-    }
+    private List<Wall> walls;
 
     public Arena(int width, int height) {
         hero = new Hero(10, 10);
         this.width = width;
         this.height = height;
+
+        this.walls = createWalls();
     }
 
     public int getWidth() {
@@ -36,10 +35,6 @@ public class Arena {
 
     public void setHeight(int height) {
         this.height = height;
-    }
-
-    private boolean canHeroMove(Position position) {
-        return position.getX() >= 0 && position.getX() < width && position.getY() >= 0 && position.getY() < height;
     }
 
     public void moveHero(Position position) {
@@ -60,5 +55,32 @@ public class Arena {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(width, height), ' ');
         hero.draw(graphics);
+
+        for(Wall wall: walls)
+            wall.draw(graphics);
+    }
+
+    private boolean canHeroMove(Position position) {
+        for(Wall wall: walls) {
+            if(position.getY() == wall.getPosition().getY() && position.getX() == wall.getPosition().getX()) return false;
+        }
+
+        return position.getX() >= 0 && position.getX() < width && position.getY() >= 0 && position.getY() < height;
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for(int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+
+        for(int r = 1; r < height -1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width-1, r));
+        }
+
+        return walls;
     }
 }
