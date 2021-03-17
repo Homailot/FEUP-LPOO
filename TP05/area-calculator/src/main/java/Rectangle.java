fbce73cd904e4d2e1ba916962acbd583ef3303a8
Nuno Alves <nyansword@gmail.com>
2021-03-17 10:18:46 +0000

    tp5: ex2 identifying the issue

---
+++ TP05/area-calculator/src/main/java/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle extends Shape {
+    private double width, height;
+
+    Rectangle(double width, double height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
