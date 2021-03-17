28a263a3c2db18750a6744b9954c85cd6a32c4c1
Nuno Alves <nyansword@gmail.com>
2021-03-17 10:13:51 +0000

    tp5: ex1 fixing the issue

--- TP05/area-calculator/src/main/java/Application.java
+++ TP05/area-calculator/src/main/java/Application.java
@@ -8,6 +8,10 @@ public class Application {
         aggregator.addShape(new Square(10));
         aggregator.addShape(new Ellipse(1,1));
 
-        System.out.println(aggregator.output());
+        AreaStringOutputter stringOutputter = new AreaStringOutputter(aggregator);
+        AreaXMLOutputter xmlOutputter = new AreaXMLOutputter(aggregator);
+
+        System.out.println(stringOutputter.output());
+        System.out.println(xmlOutputter.output());
     }
 }
