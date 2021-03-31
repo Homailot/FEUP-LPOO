69c497714f331789e4ac09d6dbcf80173ada272b
Nuno Alves <nyansword@gmail.com>
2021-03-31 18:08:57 +0100

    add: tp6 factory

---
+++ TP06/src/main/java/AlienClient.java
@@ -0,0 +1,24 @@
+public abstract class AlienClient implements Client {
+    private OrderingStrategy strategy;
+
+    public AlienClient() {
+        this.strategy = createOrderingStrategy();
+    }
+
+    @Override
+    public void happyHourEnded(Bar bar) {
+        strategy.happyHourEnded(bar);
+    }
+
+    @Override
+    public void happyHourStarted(Bar bar) {
+        strategy.happyHourStarted(bar);
+    }
+
+    @Override
+    public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
+        strategy.wants(drink, recipe, bar);
+    }
+
+    protected abstract OrderingStrategy createOrderingStrategy();
+}
